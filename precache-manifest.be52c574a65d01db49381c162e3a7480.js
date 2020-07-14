af1c8fe58680452e5f7899f96554024e5f7f7019
FelixBanan <FelixASD@ya.ru>
2020-07-15 02:19:30 +0300

    Updates

---
+++ precache-manifest.be52c574a65d01db49381c162e3a7480.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867e05fba000fcb5ed2cb1f122d452da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f96765cfdbefaf9180d4",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "8e8a029ca4ff57b49aeb",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f96765cfdbefaf9180d4",
+    "url": "./static/js/2.b0e0fa64.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.b0e0fa64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8a029ca4ff57b49aeb",
+    "url": "./static/js/main.981a9d43.chunk.js"
+  },
+  {
+    "revision": "072cfa9dd3da845fa697",
+    "url": "./static/js/runtime-main.9d9fd10a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b886236b4e2ce5870abcce5ff1c0d691.js"
+  "./precache-manifest.be52c574a65d01db49381c162e3a7480.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
